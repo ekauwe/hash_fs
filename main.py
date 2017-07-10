afd257831ca293bef6b3de0dbb4e7b395beb021e
Ernest Kauwe <ekauwe@gmail.com>
2017-07-10 19:28:27 -0400

    Edit: reformat and used generic usage

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ from hashlib import md5, sha1
 import os
 import sys
 
+def check():
+    try:
+        hashes = hashFiles(sys.argv[1])
+        print hashes
+    except:
+            usage()
+
+
+def usage():
+    print '''
+        Usage: 
+            ./%s <dir>
+        ''' % sys.argv[0]
+
 def hashFiles(_dir):
-	hashDict = {}
-	for dirName, subDir, fileList in os.walk(_dir):
-		for _file in fileList:
-			hfile  = open(dirName+'/'+_file,'r')
-			hashDict[dirName+'/'+_file] = md5(hfile.read()).hexdigest()
-			hfile.close()
-	return hashDict
+    hashDict = {}
+    for dirName, subDir, fileList in os.walk(_dir):
+        for _file in fileList:
+            hfile  = open(dirName+'/'+_file,'r')
+            hashDict[dirName+'/'+_file] = md5(hfile.read()).hexdigest()
+            hfile.close()
+    return hashDict
 
 if __name__ == '__main__':
-	hashes = hashFiles(sys.argv[1])
-
-print hashes
+    check()
