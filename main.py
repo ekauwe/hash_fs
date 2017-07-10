1d52d73728af28a5e61564b71406cec13d487d0e
Ernest Kauwe <ekauwe@gmail.com>
2017-07-10 19:06:53 -0400

    Edit: def hashFiles

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from hashlib import md5, sha1
 import os
 import sys
 
-hashDict = {}
+def hashFiles(_dir):
+	hashDict = {}
+	for dirName, subDir, fileList in os.walk(_dir):
+		for _file in fileList:
+			hfile  = open(dirName+'/'+_file,'r')
+			hashDict[dirName+'/'+_file] = md5(hfile.read()).hexdigest()
+			hfile.close()
+	return hashDict
 
-for dirName, subDir, fileList in os.walk(sys.argv[1])
-	for _file in fileList:
-		hfile  = open(dirName+'/'+_file,'r')
-		hashDict[dirName+'/'+_file] = md5(hfile.read()).hexdigest()
-		hfile.close()
-print hashDict
+if __name__ == '__main__':
+	hashes = hashFiles(sys.argv[1])
+
+print hashes
