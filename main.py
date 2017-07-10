b182322b5df942f565a10d6f49ab9e22d86901e6
Ernest Kauwe <ekauwe@gmail.com>
2017-07-10 19:02:26 -0400

    Edit: Takes dir path from cmdline arg

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from hashlib import md5, sha1
 import os
+import sys
 
 hashDict = {}
 
-for dirName, subDir, fileList in os.walk('/home/ekauwe/Downloads/All Tool/'):
+for dirName, subDir, fileList in os.walk(sys.argv[1])
 	for _file in fileList:
 		hfile  = open(dirName+'/'+_file,'r')
 		hashDict[dirName+'/'+_file] = md5(hfile.read()).hexdigest()
