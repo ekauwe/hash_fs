8e10ea19a567c9d22fbc701eb4d446bf5ab797f7
Ernest Kauwe <ekauwe@gmail.com>
2017-07-11 17:25:20 -0400

    Updated: sql query to include more columns

--- database.py
+++ database.py
@@ -4,15 +4,16 @@ import sqlite3
 def Init(db_name):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS file_hashes (date, file, md5Hash, sha1Hash, sha256Hash)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS file_hashes (dtime, dir, file, fullpath, md5Hash, sha1Hash, sha256Hash, fsize)''')
     conn.commit()
     return conn
 
 def InsertData(conn, hashDict):
     c = conn.cursor()
     for key in hashDict:
-        query = "INSERT INTO file_hashes VALUES ('%s', '%s', '%s', '%s', 0)" % (hashDict[key]['time'], key, hashDict[key]['md5'], hashDict[key]['sha1'])
-        #print query
+        #sqldata: {timestamp, dir, file, fullpath, md5, sha1, sha256, size}
+        query = "INSERT INTO file_hashes VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (hashDict[key]['time'], hashDict[key]['dir'], hashDict[key]['file'], key, hashDict[key]['md5'], hashDict[key]['sha1'], hashDict[key]['sha256'], hashDict[key]['size'])
+        
         c.execute(query)
         conn.commit()
     return 0
