c0092bfcf9df98ad83eb00f43042eb6b049bff40
Ernest Kauwe <ekauwe@gmail.com>
2017-07-13 17:42:10 -0400

    changed to a class

--- database.py
+++ database.py
@@ -1,25 +1,26 @@
 #database.py
 import sqlite3
 
-def Init(db_name):
-    conn = sqlite3.connect(db_name)
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS file_hashes (dtime, dir, file, fullpath, md5Hash, sha1Hash, sha256Hash, fsize, hits, av)''')
-    conn.commit()
-    return conn
-
-def InsertData(conn, hashDict):
-    c = conn.cursor()
-    for key in hashDict:
-        #sqldata: {timestamp, dir, file, fullpath, md5, sha1, sha256, size}
-        query = "INSERT INTO file_hashes VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '[]', 'false')" % (hashDict[key]['time'], hashDict[key]['dir'], hashDict[key]['file'], key, hashDict[key]['md5'], hashDict[key]['sha1'], hashDict[key]['sha256'], hashDict[key]['size'])
+class Sql_hashfs(object):
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        c = self.conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS file_hashes (dtime, dir, file, fullpath, md5Hash, sha1Hash, sha256Hash, fsize, hits, av)''')
+        self.conn.commit()
+        return None
+    
+    def InsertData(self, hashDict):
+        c = self.conn.cursor()
+        for key in hashDict:
+            #sqldata: {timestamp, dir, file, fullpath, md5, sha1, sha256, size}
+            query = "INSERT INTO file_hashes VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '[]', 'false')" % (hashDict[key]['time'], hashDict[key]['dir'], hashDict[key]['file'], key, hashDict[key]['md5'], hashDict[key]['sha1'], hashDict[key]['sha256'], hashDict[key]['size'])
+            c.execute(query)
+            self.conn.commit()
+        return 0
+    
+    def VtInsert(self, data):
+        c = self.conn.cursor()
+        query = "UPDATE file_hashes set hits = \"%s\", av = \"%s\" WHERE sha256Hash='%s'" % (data[0], data[1], data[2])
         c.execute(query)
-        conn.commit()
-    return 0
-
-def VtInsert(conn, data):
-    c = conn.cursor()
-    query = "UPDATE file_hashes set hits = \"%s\", av = \"%s\" WHERE sha256Hash='%s'" % (data[0], data[1], data[2])
-    c.execute(query)
-    conn.commit()
-    return 0
+        self.conn.commit()
+        return 0
